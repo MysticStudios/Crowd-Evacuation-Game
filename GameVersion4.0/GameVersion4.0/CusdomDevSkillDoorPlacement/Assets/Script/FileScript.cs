049cb8f692d59a74114f9ea92697a6d17cc2c1a4
nilayc2012 <nilay.chakraborty@rutgers.edu>
2016-03-27 18:53:39 -0400

    Latest

--- GameVersion4.0/GameVersion4.0/CusdomDevSkillDoorPlacement/Assets/Script/FileScript.cs
+++ GameVersion4.0/GameVersion4.0/CusdomDevSkillDoorPlacement/Assets/Script/FileScript.cs
@@ -39,219 +39,21 @@ public class FileScript : MonoBehaviour
         {
 
             flag = false;
-            /*if (!File.Exists("C:\\Users\\Public\\Documents\\Unity Projects\\GameVersion4.0\\CusdomDevSkillDoorPlacement\\Assets\\XMLDocs\\" + SceneManager.GetActiveScene().name + ".xml"))
-            {
-                FileStream s = new FileStream("C:\\Users\\Public\\Documents\\Unity Projects\\GameVersion4.0\\CusdomDevSkillDoorPlacement\\Assets\\XMLDocs\\" + SceneManager.GetActiveScene().name + ".xml", FileMode.Append, FileAccess.Write);
-                XmlTextWriter xwriter = new XmlTextWriter(s, Encoding.UTF8);
-                xwriter.Formatting = Formatting.Indented;
-                xwriter.WriteStartDocument();
-                xwriter.WriteStartElement("document");
-                xwriter.WriteStartElement("User-Data");
-                xwriter.WriteStartElement("Player-ID");
-                xwriter.WriteString(nameStore.name);
-                xwriter.WriteEndElement();
-                xwriter.WriteStartElement("Walls");
-
-                int count = 1;
-                foreach (Transform wall in tempListList[0])
-                {
-                    xwriter.WriteStartElement("Wall");
-                    xwriter.WriteStartElement("Wall-No");
-                    xwriter.WriteString(count.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Position");
-                    xwriter.WriteStartElement("X");
-                    xwriter.WriteString(wall.position.x.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Y");
-                    xwriter.WriteString(wall.position.y.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Z");
-                    xwriter.WriteString(wall.position.z.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteEndElement();
-
-                    xwriter.WriteStartElement("Rotation");
-                    xwriter.WriteStartElement("X");
-                    xwriter.WriteString(wall.rotation.x.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Y");
-                    xwriter.WriteString(wall.rotation.y.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Z");
-                    xwriter.WriteString(wall.rotation.z.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteEndElement();
-
-                    xwriter.WriteStartElement("Scale");
-                    xwriter.WriteStartElement("X");
-                    xwriter.WriteString(wall.localScale.x.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Y");
-                    xwriter.WriteString(wall.localScale.y.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Z");
-                    xwriter.WriteString(wall.localScale.z.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteEndElement();
-                    xwriter.WriteEndElement();
-                    count++;
-                }
-
-                xwriter.WriteEndElement();
-
-                count = 0;
-                xwriter.WriteStartElement("Doors");
-
-                foreach (Transform door in tempListList[1])
-                {
-                    xwriter.WriteStartElement("Door");
-                    xwriter.WriteStartElement("Door-No");
-                    xwriter.WriteString(count.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Position");
-                    xwriter.WriteStartElement("X");
-                    xwriter.WriteString(door.position.x.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Y");
-                    xwriter.WriteString(door.position.y.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Z");
-                    xwriter.WriteString(door.position.z.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteEndElement();
-
-                    xwriter.WriteStartElement("Rotation");
-                    xwriter.WriteStartElement("X");
-                    xwriter.WriteString(door.rotation.x.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Y");
-                    xwriter.WriteString(door.rotation.y.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Z");
-                    xwriter.WriteString(door.rotation.z.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteEndElement();
-
-                    xwriter.WriteStartElement("Scale");
-                    xwriter.WriteStartElement("X");
-                    xwriter.WriteString(door.localScale.x.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Y");
-                    xwriter.WriteString(door.localScale.y.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Z");
-                    xwriter.WriteString(door.localScale.z.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteEndElement();
-                    xwriter.WriteEndElement();
-                    count++;
-                }
-                xwriter.WriteEndElement();
-
-                count = 0;
-                xwriter.WriteStartElement("Pillars");
-
-                foreach (Transform pillar in tempListList[2])
-                {
-                    xwriter.WriteStartElement("Pillar");
-                    xwriter.WriteStartElement("Pillar-No");
-                    xwriter.WriteString(count.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Position");
-                    xwriter.WriteStartElement("X");
-                    xwriter.WriteString(pillar.position.x.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Y");
-                    xwriter.WriteString(pillar.position.y.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Z");
-                    xwriter.WriteString(pillar.position.z.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteEndElement();
-
-                    xwriter.WriteStartElement("Rotation");
-                    xwriter.WriteStartElement("X");
-                    xwriter.WriteString(pillar.rotation.x.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Y");
-                    xwriter.WriteString(pillar.rotation.y.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Z");
-                    xwriter.WriteString(pillar.rotation.z.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteEndElement();
-
-                    xwriter.WriteStartElement("Scale");
-                    xwriter.WriteStartElement("X");
-                    xwriter.WriteString(pillar.localScale.x.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Y");
-                    xwriter.WriteString(pillar.localScale.y.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Z");
-                    xwriter.WriteString(pillar.localScale.z.ToString());
-                    xwriter.WriteEndElement();
-                    xwriter.WriteEndElement();
-                    xwriter.WriteEndElement();
-                    count++;
-                }
-                xwriter.WriteEndElement();
-                //-----------------each color element-----------------------------------
-                xwriter.WriteStartElement("Heatmap");
-                Dictionary<Vector2, float> points = Camera.main.GetComponent<collectResults>().allData;
-                foreach (KeyValuePair<Vector2, float> keyval in points)
-                {
-                    xwriter.WriteStartElement("Point");
-                    xwriter.WriteStartElement("Position");
-                    xwriter.WriteString("(" + keyval.Key.x + "," + keyval.Key.y + ")");
-                    xwriter.WriteEndElement();
-                    xwriter.WriteStartElement("Value");
-                    xwriter.WriteString(""+keyval.Value);
-                    xwriter.WriteEndElement();
-                    xwriter.WriteEndElement();
-                }
-                xwriter.WriteEndElement();
-
-                xwriter.WriteStartElement("UsefulInfo");
-                xwriter.WriteStartElement("AmountOfPeopleEscaped");
-                xwriter.WriteString("" + (GameController.maxNumberPeople - GameController.numberLeft));
-                xwriter.WriteEndElement();
-                xwriter.WriteStartElement("AmountOfPeopleLeft");
-                xwriter.WriteString("" + GameController.numberLeft);
-                xwriter.WriteEndElement();
-                xwriter.WriteStartElement("totalEscapeTime");
-                xwriter.WriteString("" + GameController.totalTime);
-                xwriter.WriteEndElement();
-                xwriter.WriteStartElement("averageEscapeTime");
-                xwriter.WriteString("" + (GameController.totalTime / (GameController.maxNumberPeople - GameController.numberLeft)));
-                xwriter.WriteEndElement();
-                xwriter.WriteEndElement();
-
-
-                //-----------------------------------------------------
-
-
-
-                xwriter.WriteStartElement("Time-Elapsed");
-                xwriter.WriteString(""+GameController.maxTime);//time.ToString());
-                xwriter.WriteEndElement();
-                xwriter.WriteEndElement();
-                xwriter.WriteEndElement();
-                xwriter.WriteEndDocument();
-
-                StreamWriter sw = new StreamWriter(s);
-                sw.WriteLine("\n");
-                xwriter.Close();
-            }
-            else
-            {*/
                 Debug.Log("no xml file yet");
-				WebClient web=new WebClient();
-				web.Credentials = new System.Net.NetworkCredential("username", "password");
-				web.DownloadFile(new Uri("https://crowdevac.com//var/www/crowdevac/XMLUserData/"+ SceneManager.GetActiveScene().name + ".xml"), SceneManager.GetActiveScene().name + ".xml");
-                XmlDocument doc = new XmlDocument();
-				doc.Load(SceneManager.GetActiveScene().name + ".xml");
+				//WebClient web=new WebClient();
+				//web.Credentials = new System.Net.NetworkCredential("username", "password");
+				//String xml=web.DownloadString(new Uri("https://crowdevac.com/XMLUserData/"+ SceneManager.GetActiveScene().name + ".xml"));
+				//web.DownloadFile(new Uri("https://crowdevac.com/XMLUserData/"+ SceneManager.GetActiveScene().name + ".xml"), SceneManager.GetActiveScene().name + ".xml");
+                //XmlDocument doc = new XmlDocument();
+				WebRequest request = WebRequest.Create("http://crowdevac.com/store_data.php?scene="+SceneManager.GetActiveScene().name);
+				request.Method="GET";
+				WebResponse response = request.GetResponse();
+				Stream stream=response.GetResponseStream();
+				StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+				String xml = reader.ReadToEnd();
+				
+				XmlDocument doc = new XmlDocument();
+				doc.LoadXml(xml);
 				XmlNode document = doc.SelectSingleNode("document");
 
                 XmlNode userdata = doc.CreateElement("User-Data");
@@ -473,8 +275,17 @@ public class FileScript : MonoBehaviour
                 XmlNode timenode = doc.CreateElement("Time-Elapsed");
                 timenode.InnerText = ""+GameController.maxTime;
                 userdata.AppendChild(timenode);
-				doc.Save(SceneManager.GetActiveScene().name + ".xml");
-				web.UploadFile ("https://crowdevac.com//var/www/crowdevac/XMLUserData/" + SceneManager.GetActiveScene ().name + ".xml","PUT", SceneManager.GetActiveScene ().name + ".xml");
+				
+					StringWriter sw = new StringWriter();
+					XmlTextWriter xw = new XmlTextWriter(sw);
+					doc.WriteTo(xw);
+					xml=sw.ToString();
+				
+				request = WebRequest.Create("http://crowdevac.com/store_data.php?scene="+SceneManager.GetActiveScene().name+"&querystring="+xml);
+				request.Method="POST";
+				response = request.GetResponse();
+				//doc.Save(SceneManager.GetActiveScene().name + ".xml");
+				//web.UploadFile ("https://crowdevac.com/XMLUserData/",SceneManager.GetActiveScene ().name + ".xml");
 			 //}
         }
     }
--- GameVersion4.0/GameVersion4.0/CusdomDevSkillDoorPlacement/Assets/Script/leaderScript.cs
+++ GameVersion4.0/GameVersion4.0/CusdomDevSkillDoorPlacement/Assets/Script/leaderScript.cs
@@ -1,7 +1,12 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 using UnityEngine.UI;
+using System.Linq;
 using System.Xml;
+using System.ComponentModel;
+using System.Text;
+using System.Net;
 using System.IO;
 
 public class leaderScript : MonoBehaviour {
@@ -18,11 +23,18 @@ public class leaderScript : MonoBehaviour {
         float x = panel.transform.position.x;
         float y = panel.transform.position.y;
 
+		for(int i=1;i<=5;i++)
+		{
+		WebRequest request = WebRequest.Create("http://crowdevac.com/store_data.php?scene=Level"+i.ToString());
+				request.Method="GET";
+				WebResponse response = request.GetResponse();
+				Stream stream=response.GetResponseStream();
+				StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+				string xml = reader.ReadToEnd();
+		
         XmlDocument doc = new XmlDocument();
 
-        if (File.Exists("C:\\Users\\NILAY\\Desktop\\GameVersion3.0\\GameVersion3.0\\CusdomDevSkillDoorPlacement\\XML Docs\\Level1.xml"))
-        {
-            doc.Load("C:\\Users\\NILAY\\Desktop\\GameVersion3.0\\GameVersion3.0\\CusdomDevSkillDoorPlacement\\XML Docs\\Level1.xml");
+            doc.LoadXml(xml);
 
             foreach(XmlNode node in doc.DocumentElement.ChildNodes)
             {
@@ -54,163 +66,12 @@ public class leaderScript : MonoBehaviour {
             newPanel.transform.parent = lbody.transform;
             newPanel.transform.position = new Vector3(x, y, 0);
             newPanel.transform.GetChild(1).gameObject.GetComponent<Text>().text = playername;
-            newPanel.transform.GetChild(2).gameObject.GetComponent<Text>().text = "Level 1";
+            newPanel.transform.GetChild(2).gameObject.GetComponent<Text>().text = "Level "+i.ToString();
             newPanel.transform.GetChild(0).gameObject.GetComponent<Text>().text = mintime.ToString();
 
             y = y - 60;
-        }
-        if (File.Exists("C:\\Users\\NILAY\\Desktop\\GameVersion3.0\\GameVersion3.0\\CusdomDevSkillDoorPlacement\\XML Docs\\Level2.xml"))
-        {
-            doc.Load("C:\\Users\\NILAY\\Desktop\\GameVersion3.0\\GameVersion3.0\\CusdomDevSkillDoorPlacement\\XML Docs\\Level2.xml");
-            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
-            {
-                float nodetime = 0.0f;
-                string tempname = "";
-                foreach (XmlNode cnode in node.ChildNodes)
-                {
-                    if (cnode.Name == "Player-ID")
-                    {
-                        tempname = cnode.InnerText;
-                    }
-                    if (cnode.Name == "Time-Elapsed")
-                    {
-                        nodetime = System.Single.Parse(cnode.InnerText);
-                    }
-
-                }
-
-                if (nodetime < mintime)
-                {
-                    mintime = nodetime;
-                    playername = tempname;
-                }
-
-            }
-            GameObject newPanel = Instantiate(panel);
-            GameObject lbody = GameObject.FindGameObjectWithTag("leaderbody");
-
-            newPanel.transform.parent = lbody.transform;
-            newPanel.transform.position = new Vector3(x, y, 0);
-            newPanel.transform.GetChild(1).gameObject.GetComponent<Text>().text = playername;
-            newPanel.transform.GetChild(2).gameObject.GetComponent<Text>().text = "Level 2";
-            newPanel.transform.GetChild(0).gameObject.GetComponent<Text>().text = mintime.ToString();
-
-
-            y = y -60;
-        }
-        if (File.Exists("C:\\Users\\NILAY\\Desktop\\GameVersion3.0\\GameVersion3.0\\CusdomDevSkillDoorPlacement\\XML Docs\\Level3.xml"))
-        {
-            doc.Load("C:\\Users\\NILAY\\Desktop\\GameVersion3.0\\GameVersion3.0\\CusdomDevSkillDoorPlacement\\XML Docs\\Level3.xml");
-            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
-            {
-                float nodetime = 0.0f;
-                string tempname = "";
-                foreach (XmlNode cnode in node.ChildNodes)
-                {
-                    if (cnode.Name == "Player-ID")
-                    {
-                        tempname = cnode.InnerText;
-                    }
-                    if (cnode.Name == "Time-Elapsed")
-                    {
-                        nodetime = System.Single.Parse(cnode.InnerText);
-                    }
-
-                }
-
-                if (nodetime < mintime)
-                {
-                    mintime = nodetime;
-                    playername = tempname;
-                }
-
-            }
-            GameObject newPanel = Instantiate(panel);
-            GameObject lbody = GameObject.FindGameObjectWithTag("leaderbody");
-
-            newPanel.transform.parent = lbody.transform;
-            newPanel.transform.position = new Vector3(x, y, 0);
-            newPanel.transform.GetChild(1).gameObject.GetComponent<Text>().text = playername;
-            newPanel.transform.GetChild(2).gameObject.GetComponent<Text>().text = "Level 3";
-            newPanel.transform.GetChild(0).gameObject.GetComponent<Text>().text = mintime.ToString();
-            y = y - 60;
-        }
-        if (File.Exists("C:\\Users\\NILAY\\Desktop\\GameVersion3.0\\GameVersion3.0\\CusdomDevSkillDoorPlacement\\XML Docs\\Level4.xml"))
-        {
-            doc.Load("C:\\Users\\NILAY\\Desktop\\GameVersion3.0\\GameVersion3.0\\CusdomDevSkillDoorPlacement\\XML Docs\\Level4.xml");
-            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
-            {
-                float nodetime = 0.0f;
-                string tempname = "";
-                foreach (XmlNode cnode in node.ChildNodes)
-                {
-                    if (cnode.Name == "Player-ID")
-                    {
-                        tempname = cnode.InnerText;
-                    }
-                    if (cnode.Name == "Time-Elapsed")
-                    {
-                        nodetime = System.Single.Parse(cnode.InnerText);
-                    }
-
-                }
-
-                if (nodetime < mintime)
-                {
-                    mintime = nodetime;
-                    playername = tempname;
-                }
-
-            }
-            GameObject newPanel = Instantiate(panel);
-            GameObject lbody = GameObject.FindGameObjectWithTag("leaderbody");
-
-            newPanel.transform.parent = lbody.transform;
-            newPanel.transform.position = new Vector3(x, y, 0);
-            newPanel.transform.GetChild(1).gameObject.GetComponent<Text>().text = playername;
-            newPanel.transform.GetChild(2).gameObject.GetComponent<Text>().text = "Level 4";
-            newPanel.transform.GetChild(0).gameObject.GetComponent<Text>().text = mintime.ToString();
-
-            y = y -60;
-        }
-        if (File.Exists("C:\\Users\\NILAY\\Desktop\\GameVersion3.0\\GameVersion3.0\\CusdomDevSkillDoorPlacement\\XML Docs\\Level5.xml"))
-        {
-            doc.Load("C:\\Users\\NILAY\\Desktop\\GameVersion3.0\\GameVersion3.0\\CusdomDevSkillDoorPlacement\\XML Docs\\Level5.xml");
-            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
-            {
-                float nodetime = 0.0f;
-                string tempname = "";
-                foreach (XmlNode cnode in node.ChildNodes)
-                {
-                    if (cnode.Name == "Player-ID")
-                    {
-                        tempname = cnode.InnerText;
-                    }
-                    if (cnode.Name == "Time-Elapsed")
-                    {
-                        nodetime = System.Single.Parse(cnode.InnerText);
-                    }
-
-                }
-
-                if (nodetime < mintime)
-                {
-                    mintime = nodetime;
-                    playername = tempname;
-                }
-
-            }
-            GameObject newPanel = Instantiate(panel);
-            GameObject lbody = GameObject.FindGameObjectWithTag("leaderbody");
-
-            newPanel.transform.parent = lbody.transform;
-            newPanel.transform.position = new Vector3(x, y, 0);
-            
-            newPanel.transform.GetChild(1).gameObject.GetComponent<Text>().text = playername;
-            newPanel.transform.GetChild(2).gameObject.GetComponent<Text>().text = "Level 5";
-            newPanel.transform.GetChild(0).gameObject.GetComponent<Text>().text = mintime.ToString();
-        }
-
+       
+		}
 
     }
 	
