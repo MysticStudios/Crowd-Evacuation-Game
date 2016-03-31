43360d5894189120e23ec1626814b33bd36e826d
nilayc2012 <nilay.chakraborty@rutgers.edu>
2016-03-30 20:31:57 -0400

    03/30

---
+++ GameVersion4.0/GameVersion4.0/CusdomDevSkillDoorPlacement/Assets/Script/AudioControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioControl : MonoBehaviour {
+
+    AudioSource audio;
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(bringUpMenu.running==1)
+        {
+            audio.Stop();
+        }
+
+	}
+}
