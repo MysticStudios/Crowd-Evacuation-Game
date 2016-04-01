894386759d575ad20dd8abb5be72d66358c14121
nilayc2012 <nilay.chakraborty@rutgers.edu>
2016-03-31 22:51:10 -0400

    Version 5 modified

---
+++ GameVersion5.0/GameVersion4.0/CusdomDevSkillDoorPlacement/Assets/Script/timer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class timer : MonoBehaviour {
+
+    public float time =0;
+
+	// Use this for initialization
+	void Start () {
+        time = 0f;
+        bringUpMenu.mytimer = 0;
+        GameController.totalTime = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        time = bringUpMenu.mytimer;//time + Time.deltaTime;
+	}
+
+    void onDisable()
+    {
+        GameController.totalTime += time;
+        Debug.Log("disabled total time:" + GameController.totalTime);
+    }
+
+}
