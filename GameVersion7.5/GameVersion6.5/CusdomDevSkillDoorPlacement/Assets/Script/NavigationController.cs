fe15b344addda23867eb30413be501ddb479643f
nilayc2012 <nilay.chakraborty@rutgers.edu>
2016-04-06 19:09:26 -0400

    Crowd simulation resolved

---
+++ GameVersion7.5/GameVersion6.5/CusdomDevSkillDoorPlacement/Assets/Script/NavigationController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class NavigationController : MonoBehaviour {
+
+    public Transform chosenTarget;
+    GameObject[] targets;
+    NavMeshAgent agent;
+    Animator anim;
+
+    static System.Random r;
+    Vector3 currentPosition;
+    public NavMeshPath path;
+    public static bool startFlag;
+    public static int value;
+    public int loc;
+    public AudioClip siren;
+
+
+    Vector2 smoothDeltaPosition = Vector2.zero;
+    Vector2 velocity = Vector2.zero;
+
+    public Vector3 startPosition;
+    // Use this for initialization
+    void Start () {
+        startFlag = false;
+        targets = GameObject.FindGameObjectsWithTag("destinations");
+        anim = GetComponent<Animator>();
+
+        agent = GetComponent<NavMeshAgent>();
+
+		
+        if(GetComponent<newagentscript>()!=null){
+		agent.enabled = false;
+
+        transform.position = startPosition;
+
+        agent.enabled = true;
+		}
+		else{
+			this.gameObject.SetActive(false);
+		}
+        /* if (targets.Length == 1)
+             loc = 1;*/
+        loc = bringUpMenu.r.Next(1, targets.Length+1);
+        chosenTarget = targets[loc - 1].transform;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+        if (bringUpMenu.running==1)
+        {
+
+                if (makeDoor.recomputeFlag)
+                {
+                    NavMesh.CalculatePath(this.gameObject.transform.position, chosenTarget.position, NavMesh.AllAreas, path);
+                    agent.path = path;
+                }
+                else if(path==null)
+                {
+                    agent.SetDestination(chosenTarget.position);
+
+                }
+            if (agent.velocity == Vector3.zero)
+                anim.SetBool("walk", false);
+            else
+            anim.SetBool("walk", true);
+
+
+        }
+
+            if (Vector3.Distance(chosenTarget.position, this.gameObject.transform.position) <= 2)
+            {
+                GameController.totalTime += this.gameObject.GetComponent<timer>().time;
+                //bringUpMenu.setTimer = false;
+                this.gameObject.SetActive(false);
+            }
+            
+
+        
+    }
+
+    void OnAnimatorMove()
+    {
+        // Update position to agent position
+        transform.position = agent.nextPosition;
+
+    }
+}
---
+++ GameVersion7.5/GameVersion6.5/CusdomDevSkillDoorPlacement/Assets/Script/doorOpeningScript.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doorOpeningScript : MonoBehaviour {
+
+    private Rigidbody rb;
+    private Vector3 previousPos;
+    private Quaternion previousRot;
+    private bool collided = false;
+    private int timer = 0;
+
+    public AudioClip openclip;
+    public AudioClip closeclip;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        collided = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (collided)
+            timer++;
+        if(timer==100)
+        {
+            timer = 0;
+            rb.transform.position = previousPos;
+            rb.transform.rotation = previousRot;
+            collided = false;
+        }
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("agent"))
+        {
+            if (collided)
+            {
+                AudioSource.PlayClipAtPoint(closeclip, other.transform.position, 1.0f);
+                rb.transform.position = previousPos;
+                rb.transform.rotation = previousRot;
+     
+                collided = false;
+            }
+            else
+            {
+                collided = true;
+                AudioSource.PlayClipAtPoint(openclip, other.transform.position, 1.0f);
+                previousPos = rb.transform.position;
+                previousRot = rb.transform.rotation;
+
+                if (rb.transform.rotation.y == 0)
+                {
+                    rb.transform.position = new Vector3(rb.transform.position.x + rb.transform.localScale.x / 2, rb.transform.position.y, rb.transform.position.z - rb.transform.localScale.x / 2);
+                    
+                }
+                else
+                {
+                    rb.transform.position = new Vector3(rb.transform.position.x - rb.transform.localScale.x / 2, rb.transform.position.y, rb.transform.position.z + rb.transform.localScale.x / 2);
+                   
+                }
+                rb.transform.Rotate(new Vector3(0, 90, 0));
+            }
+        }
+
+    }
+}
