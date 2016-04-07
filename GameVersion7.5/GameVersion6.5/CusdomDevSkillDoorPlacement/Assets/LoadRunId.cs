23affa578574eede7cb5cda4d72fac655ea3384b
nilayc2012 <nilay.chakraborty@rutgers.edu>
2016-04-06 20:26:51 -0400

    web build: crowd simulation resolved

--- GameVersion7.5/GameVersion6.5/CusdomDevSkillDoorPlacement/Assets/LoadRunId.cs
+++ GameVersion7.5/GameVersion6.5/CusdomDevSkillDoorPlacement/Assets/LoadRunId.cs
@@ -4,14 +4,14 @@ using System.Collections;
 public class LoadRunId : MonoBehaviour {
 
 	// Use this for initialization
-	/*IEnumerator Start () {
+	IEnumerator Start () {
 	
 		string url="http://crowdevac.com/get_run_id.php?id=1";
 		//string url="http://localhost/get_run_id.php?id=1";
 				WWW www = new WWW(url);
 				yield return www;
 				nameStore.runId=www.text;
-	}*/
+	}
 	
 	// Update is called once per frame
 	void Update () {
