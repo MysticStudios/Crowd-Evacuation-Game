e99942131b96614efeff2899cb41a8fca6e07533
nilayc2012 <nilay.chakraborty@rutgers.edu>
2016-04-23 17:59:45 -0400

    LOA and HOMOG

---
+++ GameVersion9.0/GameVersion8.5/Assets/Script/MidModalPanel.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+public class MidModalPanel : MonoBehaviour {
+    public Button cont;
+    public Button restart;
+    public Button quit;
+
+    public GameObject modalPanelObject;
+
+    private static MidModalPanel modalPanel;
+
+    public static MidModalPanel Instance()
+    {
+        if(!modalPanel)
+        {
+            modalPanel = FindObjectOfType(typeof(MidModalPanel)) as MidModalPanel;
+            if (!modalPanel)
+                Debug.LogError("one active modal panel script needed");
+        }
+        return modalPanel;
+    }
+
+    public void Choice(UnityAction repEvent, UnityAction qEvent)
+    {
+        modalPanelObject.SetActive(true);
+        cont.onClick.RemoveAllListeners();
+        cont.onClick.AddListener(ClosePanel);
+
+        restart.onClick.RemoveAllListeners();
+        restart.onClick.AddListener(repEvent);
+
+        quit.onClick.RemoveAllListeners();
+        quit.onClick.AddListener(qEvent);
+
+
+        cont.gameObject.SetActive(true);
+        restart.gameObject.SetActive(true);
+        quit.gameObject.SetActive(true);
+    }
+    public void ClosePanel()
+    {
+        bringUpMenu.pause = false;
+        if(!bringUpMenu.crowdflag)
+        bringUpMenu.noList = false;
+        else
+        {
+            GameObject.Find("Canvascrowd").GetComponent<Canvas>().enabled=true;
+        }
+        modalPanelObject.SetActive(false);
+    }
+}
+
+
+
+
