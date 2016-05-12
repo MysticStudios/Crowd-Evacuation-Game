eb0b0e95e6122b38f7d36782f36122433cb33103
Piccirello <Piccirello@users.noreply.github.com>
2016-05-12 17:11:04 -0400

    Update getheatmapdata.php
    echo XML of User-Data with minimal time
    Change GET variables to POST
    Fix spacing

--- crowdevac.com/getheatmapdata.php
+++ crowdevac.com/getheatmapdata.php
@@ -1,41 +1,45 @@
 <?php
-	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-		if (!isset($_GET['scene']) || empty($_GET['scene'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	//header('Content-Type: text/html');
+	if (!isset($_POST['scene']) || empty($_POST['scene'])) {
 		exit(0);
 	}
-	
-	$scene = filterAlphanumeric($_GET["scene"]);
-	$loa=filterAlphanumeric($_GET["loa"]);
-	$los=filterAlphanumeric($_GET["los"]);
-	$homo=filterAlphanumeric($_GET["homo"]);
-	
+
+	$scene = filterAlphanumeric($_POST["scene"]);
+	$loa=filterAlphanumeric($_POST["loa"]);
+	$los=filterAlphanumeric($_POST["los"]);
+	$homo=filterAlphanumeric($_POST["homo"]);
+
 	// create XML file for current scvene if it doesn't yet exist
 	if (!file_exists("./XMLDocs/". $scene . ".xml"))	{
 		createXMLFile($scene);
 	}
-	
+
 	$xmlfile = fopen("./XMLDocs/". $scene . ".xml", "r");
 	$xmlstring = fread($xmlfile,filesize("./XMLDocs/" . $scene . ".xml"));
-	
-	$mintime=INF;
+
+	$mintime=PHP_INT_MAX;
 	$leader="";
-	$loadedxml=simplexml_load_file("./XMLDocs/" . $scene . ".xml");
-	
+        $loadedxml=simplexml_load_file("./XMLDocs/" . $scene . ".xml");
+	$minXML = "";
+
 	foreach ($loadedxml as $userdata):
-        $leaderdata=$userdata->{"User-Data"};
-        $telap=floatval($userdata->{"Time-Elapsed"});
-		$tlos=$userdata->{'LevelOfService'};
-		$thomo=$userdata->{'Homogeneity'};
-		$tloa=$userdata->{'LevelOfAggression'};
-		
-		if($telap<=$mintime && $tloa==$loa &&$tlos==$los && $homo==$thomo)
-		{
-			$mintime=$telap;
-			$leader=$leaderdata;
+		if (empty($minXML)) {
+			$minXML = $userdata;
 		}
-    endforeach;
-	
-	echo $leader;
+
+	        $telap=floatval($userdata->{"Time-Elapsed"});
+		$tlos=$userdata->{"LevelOfService"};
+		$thomo=$userdata->{"Homogeneity"};
+		$tloa=$userdata->{"LevelOfAggression"};
+
+		if($telap<=$mintime && $tloa==$loa && $tlos==$los && $homo==$thomo) {
+			$mintime=$telap;
+			$minXML = $userdata;
+		}
+	endforeach;
+
+	echo $minXML->asXML();
 }
 
 // removes all non-alphanumeric characters
