645c92e7e21f6e725291e930e2795c27f218d71a
Piccirello <Piccirello@users.noreply.github.com>
2016-05-13 03:08:20 -0400

    Download php files after PUSH

--- crowdevac.com/getLatest.php
+++ crowdevac.com/getLatest.php
@@ -1,10 +1,8 @@
 <?php
-$value = file_put_contents("web demo.unity3d", fopen("https://github.com/MysticStudios/Crowd-Evacuation-Game/raw/master/crowdevac.com/web%20demo.unity3d", 'r'));
-if ($value === false || $value === 0) {
-        echo "<pre>Download FAILED</pre>";
-}
-else {
-        echo "<pre>Download was successful!</pre>";
-        echo "<pre>File size: " . round($value/1000000, 1) . "MB</pre>";
-}
+file_put_contents("web demo.unity3d", fopen("https://github.com/MysticStudios/Crowd-Evacuation-Game/raw/master/crowdevac.com/web%20demo.unity3d", 'r'));
+file_put_contents("get_image.php", fopen("https://github.com/MysticStudios/Crowd-Evacuation-Game/raw/master/crowdevac.com/get_image.php", 'r'));
+file_put_contents("get_run_id.php", fopen("https://github.com/MysticStudios/Crowd-Evacuation-Game/raw/master/crowdevac.com/get_run_id.php", 'r'));
+file_put_contents("getheatmapdata.php", fopen("https://github.com/MysticStudios/Crowd-Evacuation-Game/raw/master/crowdevac.com/getheatmapdata.php", 'r'));
+file_put_contents("store_data.php", fopen("https://github.com/MysticStudios/Crowd-Evacuation-Game/raw/master/crowdevac.com/store_data.php", 'r'));
+file_put_contents("store_image.php", fopen("https://github.com/MysticStudios/Crowd-Evacuation-Game/raw/master/crowdevac.com/store_image.php", 'r'));
 ?>
