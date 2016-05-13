c710f9f55f5a3f95c9a31343c28f094227569a7e
nilayc2012 <nilay.chakraborty@rutgers.edu>
2016-05-13 10:46:25 -0400

    updated

--- crowdevac.com/getheatmapdata.php
+++ crowdevac.com/getheatmapdata.php
@@ -28,6 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		$tlos=$userdata->{"LevelOfService"};
 		$thomo=$userdata->{"Homogeneity"};
 		$tloa=$userdata->{"LevelOfAggression"};
+		
 		if($telap<=$mintime && $tloa==$loa && substr($tlos,0, 1)==substr($los,0, 1) && $homo==$thomo) {
 			$mintime=$telap;
 			$minXML = $userdata;
